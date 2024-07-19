1df64b01088fb90fd7b8ad8aaac50f02af57b04d
blakehulett7 <blake.hulett7@gmail.com>
2024-07-19 17:45:45 -0500

    facedown plays mod added

--- scoremods.go
+++ scoremods.go
@@ -29,37 +29,76 @@ func (d Duel) effectiveAttacksMod() int {
 		fmt.Println("Effective Attacks are somehow negative, fix!!")
 		return 0
 	}
-	if d.turns < 2 {
+	if d.effectiveAttacks < 2 {
 		return 4
 	}
-	if d.turns < 4 {
+	if d.effectiveAttacks < 4 {
 		return 2
 	}
-	if d.turns < 10 {
+	if d.effectiveAttacks < 10 {
 		return 0
 	}
-	if d.turns < 20 {
+	if d.effectiveAttacks < 20 {
 		return -2
 	}
 	return -4
 }
 
 func (d Duel) defensiveWinsMod() int {
-	if d.effectiveAttacks < 0 {
+	if d.defensiveWins < 0 {
 		fmt.Println("Defensive Wins are somehow negative, fix!!")
 		return 0
 	}
-	if d.turns < 2 {
+	if d.defensiveWins < 2 {
 		return 0
 	}
-	if d.turns < 6 {
+	if d.defensiveWins < 6 {
 		return -10
 	}
-	if d.turns < 10 {
+	if d.defensiveWins < 10 {
 		return -20
 	}
-	if d.turns < 15 {
+	if d.defensiveWins < 15 {
 		return -30
 	}
 	return -40
 }
+
+func (d Duel) facedownPlaysMod() int {
+	if d.facedownPlays < 0 {
+		fmt.Println("Facedown Plays are somehow negative, fix!!")
+		return 0
+	}
+	if d.facedownPlays < 1 {
+		return 0
+	}
+	if d.facedownPlays < 11 {
+		return -2
+	}
+	if d.facedownPlays < 21 {
+		return -4
+	}
+	if d.facedownPlays < 31 {
+		return -6
+	}
+	return -8
+}
+
+func (d Duel) facedownPlaysMod() int {
+	if d.facedownPlays < 0 {
+		fmt.Println("Facedown Plays are somehow negative, fix!!")
+		return 0
+	}
+	if d.facedownPlays < 1 {
+		return 0
+	}
+	if d.facedownPlays < 11 {
+		return -2
+	}
+	if d.facedownPlays < 21 {
+		return -4
+	}
+	if d.facedownPlays < 31 {
+		return -6
+	}
+	return -8
